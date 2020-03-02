e0cb10e3069901bd47eaa9a63e1bbb5ffb3a3408
tammabathular <tammabathular@KC-ISIA-12TJG72>
2020-03-01 22:03:58 -0600

    first commit

---
+++ pickanddrop/src/main/java/edu/umkc/tammabathular/pickanddrop/PickAndDropApplication.java
@@ -0,0 +1,13 @@
+package edu.umkc.tammabathular.pickanddrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickAndDropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickAndDropApplication.class, args);
+	}
+
+}
---
+++ pickanddrop/src/test/java/edu/umkc/tammabathular/pickanddrop/PickAndDropApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.umkc.tammabathular.pickanddrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickAndDropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
